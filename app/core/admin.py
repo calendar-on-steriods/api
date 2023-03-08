daaf6344ad8fe87e41e513080bb9e2a9aa7333ec
Olúwatìmìlé̩hìn Ako̩gun <hello@timiakogun.com>
2023-03-08 09:36:35 +0100

    feat: Allow user creation on admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,52 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['last_login']
+    list_display = ['email', 'full_name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('first_name', "last_name")}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'first_name',
+                'last_name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+    def full_name(self, obj):
+        return f'{obj.first_name}, {obj.last_name}'
+
+
+admin.site.register(models.User, UserAdmin)
